d300598ea02fad75a1ea55bb137e05541ca064e5
“Christopher <christopherhileweb@gmail.com>
2023-01-16 09:17:38 -0500

    cart context and shopping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Authentication from './routes/authentication/authentication.component';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
+import Shop from './routes/shop/shop.component';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Navigation />}>
           <Route index element={<Home />} />
+          <Route path='shop' element={<Shop />} />
           <Route path='auth' element={<Authentication />} />
         </Route>
       </Routes>
